945ff3fda98a6862fff43003c3fed7a778a60089
SergeyVoronkov7707 <59518941+SergeyVoronkov7707@users.noreply.github.com>
2020-06-25 16:09:31 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,37 +1,16 @@
-# AIzaSyAPBPZIszn4zioqSsqjk7cHKIC7YZXR4cI
-
-
 import json
 import requests
+import config
 from pprint import pprint
 
-main_link = 'https://www.googleapis.com/youtube/v3/subscriptions'
+main_link = 'https://www.googleapis.com/youtube/v3/channels'
 
-
-user = 'SergeyVoronkov7707'
-key = 'AIzaSyAPBPZIszn4zioqSsqjk7cHKIC7YZXR4cI'
-
-params = {'key': key, 'part': 'snippet', 'categoryId': 'Video'}
+key = config.key
+params = {'key': key, 'part': 'snippet', 'contentDetails':'statistics','forUsername': 'GoogleDevelopers'}
 
 response = requests.get(main_link, params=params)
 data = response.json()
 pprint(data)
-# log_web = requests.get('https://www.googleapis.com/youtube/v3/subscriptions')
 
-
-# repo = []
-#
-# js = log_web.json()
-#
-# if len(js) != 0:
-#     print("Список репозиториев пользователя:")
-# else:
-#     print("У пользователя нет репозиториев.")
-#
-# for js_i in js:
-# #Отобраны репозитории в котрых пользователь является владельцем
-#     if js_i['owner']['login'] == user:
-#         repo.append(js_i['name'])
-#         print(f"{len(repo)}. {js_i['name']}")
-#         with open('gittext.json', 'w') as f:
-#             json.dump(repo, f)
+with open('youresponse.json', 'w') as f:
+    json.dump(data, f)
