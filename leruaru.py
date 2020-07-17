0528b7027b225b3c80af38c6b5b9188c620e23e9
SergeyVoronkov7707 <59518941+SergeyVoronkov7707@users.noreply.github.com>
2020-07-18 02:58:57 +0300

    Update leruaru.py

--- leruaru.py
+++ leruaru.py
@@ -8,13 +8,10 @@ class LeruaruSpider(scrapy.Spider):
     name = 'leruaru'
     allowed_domains = ['leroymerlin.ru']
 
-
     def __init__(self, search):
         self.start_urls = [f'https://leroymerlin.ru/search/?q={search}&suggest=true']
 
-    def parse(self, response:HtmlResponse):
-        # next_page = response.xpath("//a[@class='paginator-button next-paginator-button']['href']").extract_first
-        # next_page = response.xpath("//a[@class='green-button btn plp-card-list__show-more-btn']").extract_first
+    def parse(self, response: HtmlResponse):
         next_page = response.css('a.paginator-button.next-paginator-button::attr(href)').extract_first()
 
         ads_links = response.xpath("//div[@class='ui-product-card']/div/div[@class='product-name']/a")
@@ -23,13 +20,15 @@ class LeruaruSpider(scrapy.Spider):
 
         yield response.follow(next_page, callback=self.parse)
 
-    def parse_ads(self, response:HtmlResponse):
+    def parse_ads(self, response: HtmlResponse):
         loader = ItemLoader(item=LeruaparserItem(), response=response)
-        loader.add_css('name','h1::text')
+        loader.add_css('name', 'h1::text')
         loader.add_xpath('price', "//span[@slot='price']/text()")
         loader.add_xpath('currency', "//span[@slot='currency']/text()")
         loader.add_xpath('num', "//span[@slot='unit']/text()")
-        loader.add_value('url',response.url)
-        loader.add_xpath('photos',"//picture/img[@alt='product image']/@src")
-        yield loader.load_item()
+        loader.add_value('url', response.url)
+        loader.add_xpath('specific', "//section[@id='nav-characteristics']//text()")
+        loader.add_xpath('photo_cat', "//uc-pdp-card-ga-enriched[@class='card-data']/uc-pdp-media-carousel/img/@src")
+        loader.add_xpath('photos', "//uc-pdp-card-ga-enriched[@class='card-data']/uc-pdp-media-carousel/img/@src")
 
+        yield loader.load_item()
