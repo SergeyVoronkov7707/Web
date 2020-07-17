5b1926b613d364aacab514c1f973cb2389264e95
SergeyVoronkov7707 <59518941+SergeyVoronkov7707@users.noreply.github.com>
2020-07-18 02:59:21 +0300

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -10,18 +10,37 @@ from pprint import pprint
 from scrapy.pipelines.images import ImagesPipeline
 import scrapy
 from pymongo import MongoClient
+import os
+from urllib.parse import urlparse
+
+
+class LeruaSpecifications:
+    def process_item(self, item, spider):
+        n = 7
+        w_desc = {}
+        item['specific'] = [line.strip() for line in item['specific']]
+        while True:
+            try:
+                w_desc[item['specific'][n]] = item['specific'][n + 2]
+                n = n + 6
+            except:
+                break
+        item['specific'] = w_desc
+        return item
+
 
 class DataBasePipeline:
     def __init__(self):
-        self.client = MongoClient('localhost',27017)
+        self.client = MongoClient('localhost', 27017)
         self.mongo_base = self.client.avito_photos
+
     def process_item(self, item, spider):
         collection = self.mongo_base[spider.name]
         collection.insert_one(item)
         return item
+
     def __del__(self):
         self.client.close()
-
 
 
 class LeruaparserPipeline:
@@ -31,6 +50,10 @@ class LeruaparserPipeline:
         pprint(item)
         return item
 
+    def item_completed(self, results, item, info):
+        if results:
+            item['photos'] = [itm[1] for itm in results if itm[0]]
+        return item
 
 
 class LeruaPhotosPipeline(ImagesPipeline):
@@ -38,12 +61,15 @@ class LeruaPhotosPipeline(ImagesPipeline):
         if item['photos']:
             for img in item['photos']:
                 try:
-                    yield scrapy.Request(img,meta=item)
+                    yield scrapy.Request(img, meta=item)
                 except Exception as e:
                     print(e)
 
-
-
+    def file_path(self, request, response=None, info=None):
+        media_dir = os.path.basename(request.meta['name'])
+        media_name = os.path.basename(urlparse(request.url).path)
+        dir = '/full/%s/%s' % (media_dir, media_name)
+        return dir
 
     def item_completed(self, results, item, info):
         if results:
