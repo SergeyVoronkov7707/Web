02156f8ed3cc5a1ace1967b647fe6123d4840292
SergeyVoronkov7707 <59518941+SergeyVoronkov7707@users.noreply.github.com>
2020-07-22 10:30:58 +0300

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -6,8 +6,26 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from pprint import pprint
+# from scrapy.pipelines.images import ImagesPipeline
+from pymongo import MongoClient
+import os
+from urllib.parse import urlparse
+import scrapy
 
 class InstaparserPipeline:
+    def __init__(self):
+        self.client = MongoClient('localhost', 27017)
+        self.mongo_base = self.client.insta_parse
+
     def process_item(self, item, spider):
+        collection = self.mongo_base[spider.name]
+        collection.insert_one(item)
+
+
         return item
+
+    def __del__(self):
+        self.client.close()
+
+
